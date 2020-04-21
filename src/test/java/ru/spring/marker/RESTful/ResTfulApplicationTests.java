e01488d001a3890228218313bd16415fdace6ad6
marker <max3472@gmail.com>
2020-04-21 10:45:48 -0700

    start

---
+++ src/test/java/ru/spring/marker/RESTful/ResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.marker.RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
